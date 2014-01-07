34a8270b7a0962c33011b3051f652662c888fc06
park sung chan <dnrmrgks@naver.com>
2014-01-07 21:43:28 +0900

    이미지기반피커
    소녀시

--- Ex.3.3.8/ViewController.m
+++ Ex.3.3.8/ViewController.m
@@ -7,13 +7,57 @@
 //
 
 #import "ViewController.h"
+#define MAX_NUM 8
+@interface ViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 
-@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
+- (IBAction)selectRandom:(id)sender {
+    
+    
+    
+    
+    int r = arc4random() % MAX_NUM;
+    [_picker selectRow:r inComponent:0 animated:YES];
+}
 
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    return 1;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    return MAX_NUM;
+}
+
+
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component{
+    return 64;
+}
+
+
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    NSString *imagePath = [NSString stringWithFormat:@"snsd%d.png",row];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    
+    UIImageView *imageView;
+    
+    if(nil == view){
+        
+        imageView = [[UIImageView alloc]initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+        
+        
+    }else{
+        
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+    }
+    return imageView;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
